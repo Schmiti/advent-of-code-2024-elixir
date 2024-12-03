430770176ecc36f186ee1f0669f8e9ac7d7922fe
Aurélien Sylvan <aurelien.sylvan@partnre.com>
2024-12-03 15:47:11 +0100

    Day 03

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,14 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  def part1(input) do
+    Regex.scan(~r/mul\((\d+),(\d+)\)/, input)
+    |> Enum.map(fn [_, a, b] -> String.to_integer(a) * String.to_integer(b) end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> String.replace("\n", "")
+    |> String.replace(~r/don't\(\)(.*)do\(\)/U, "")
+    |> part1()
   end
 end
